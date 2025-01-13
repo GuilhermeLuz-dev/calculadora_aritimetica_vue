62d16ba1c8f1a456f8827a55d5d4a37348b20e89
Guilherme Rocha <luzguilherme.dev@gmail.com>
2025-01-13 16:16:01 -0300

    feature: desenvolvendo logica de seleção do operator

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { reactive } from 'vue';
+import Header from './components/Header.vue';
+import Calculadora from './components/Calculadora.vue';
+
+const state = reactive({
+  operator: 'soma'
+})
+
+function getOperator() {
+  switch (state.operator) {
+    case 'soma':
+      return '+'
+    case 'diferenca':
+      return '-'
+    case 'multiplicacao':
+      return 'X'
+    default:
+      return '/'
+  }
+}
+</script>
+<template>
+  <div class="background">
+    <div class="container">
+      <Header :editarOperator="event => state.operator = event.target.value"></Header>
+      <Calculadora :signal="getOperator()"></Calculadora>
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  color: #fff;
+}
+
+.background {
+  width: 100%;
+  height: 100vh;
+  background-color: #000;
+}
+
+.container {
+  width: 100%;
+  max-width: 674px;
+  display: block;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['signal'])
+</script>
+<template>
+    <div class="calculadora">
+        <input type="number" id="firstNumber">
+        <span id="signal">
+            {{ props.signal }}
+        </span>
+        <input type="number" id="secundNumber">
+        <span id="equals">=</span>
+        <div id="result"></div>
+    </div>
+</template>
+<style scoped>
+#result {
+    width: 50px;
+    height: 5%;
+    background-color: #fff;
+}
+</style>
